8be835b85092e33be82466d1ca614de579796cd1
Liwanx222 <79998147+Liwanx222@users.noreply.github.com>
2022-05-24 20:56:35 +0200

    directories made

---
+++ views/app.js
@@ -0,0 +1,11 @@
+let fs = require("fs");
+let express = require("express");
+
+const app = express();
+app.set("view engine", "ejs");
+const port = 8080;
+
+
+app.get("/", (req, res) => {
+    res.render("pages/index");
+})
